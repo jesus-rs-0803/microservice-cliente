3a3b281f94eac382599503179927fec37522ffab
jesus-rs-0803 <jesus.enggel.rivera.santiago@everis.com>
2020-07-20 17:43:20 -0500

    Subida fuente

--- src/main/java/com/bootcam/repositories/ClientRepository.java
+++ src/main/java/com/bootcam/repositories/ClientRepository.java
@@ -1,8 +1,13 @@
 package com.bootcam.repositories;
 
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.bootcam.entities.Client;
+
 @Repository
-public interface ClientRepository {
+public interface ClientRepository extends MongoRepository<Client, String> {
+	
+	
 
 }
